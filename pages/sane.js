7cacac5928fd0c57689c81f4f52a62378c5a9562
W Rivera <wrivera@Ws-iMac.fios-router.home>
2021-09-06 09:44:42 -0400

    gsap sane page

--- pages/sane.js
+++ pages/sane.js
@@ -2,7 +2,6 @@ import  { useRef, useEffect } from "react";
 import {  gsap } from "gsap";
 import ScrollTrigger from "gsap/dist/ScrollTrigger";
 
-import styles from "/App.module.css";
 import Workout from './workout.svg';
 import Happy from './happy.svg';
 import Greensocklogo from './greensocklogo.svg';
