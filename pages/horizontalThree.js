cd0658559c5f32f381c9779592c69f00a2ce6b62
W Rivera <wrivera@Ws-iMac.fios-router.home>
2021-09-14 01:20:50 -0400

    horizontalThree

---
+++ pages/horizontalThree.js
@@ -0,0 +1,66 @@
+ import gsap from 'gsap'
+import { useEffect } from 'react';
+import ScrollTrigger from "gsap/dist/ScrollTrigger";
+
+{/*Reference https://codepen.io/GreenSock/pen/JjYPgdp */}
+ export default function Some() {
+ 
+  useEffect(() => {
+    gsap.registerPlugin(ScrollTrigger);
+
+    gsap.registerPlugin(ScrollTrigger);
+
+    gsap.set(".massiveImage", {backgroundImage: `url(https://source.unsplash.com/random/${innerWidth * 3}x${innerHeight})`})
+    
+    gsap.to(".massiveImage", {
+      xPercent: -100, 
+      x: () => innerWidth,
+      ease: "none",
+      scrollTrigger: {
+        trigger: ".massiveImage",
+        start: "top top",
+        end: () => innerWidth * 3,
+        scrub: true,
+        pin: true,
+        invalidateOnRefresh: true,
+        anticipatePin: 1
+      }
+    });
+    
+    
+}, []);
+
+
+  return (
+    <>
+<div> 
+<h1 className="header-section">Scroll down to see a horizontal scroll section</h1>
+  
+<div className="massiveImage"></div>
+
+<h1 className="header-section">Now we're back to regular scrolling</h1>
+
+
+
+
+ 
+
+     <style
+      dangerouslySetInnerHTML={{
+          __html: `
+          .massiveImage {
+            height: 100vh;
+            width: 400vw;
+            background-size: cover;
+            background-position: center;
+          }
+    
+          
+          
+                   `
+      }}
+    />
+    </div>
+ </>
+ )
+}
