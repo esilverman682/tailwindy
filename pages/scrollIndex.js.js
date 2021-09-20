01739c078f1c3238a7ea3db1ff8c2602b93a701b
W Rivera <wrivera@Ws-iMac.fios-router.home>
2021-09-20 18:32:27 -0400

    locoFinally

--- pages/scrollIndex.js.js
+++ pages/scrollIndex.js.js
@@ -1,7 +1,7 @@
 /* eslint-disable @next/next/no-img-element */
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
-import "./styles.css";
+//import "./styles.css";
 const slideData = [
   {
     index: 0,
