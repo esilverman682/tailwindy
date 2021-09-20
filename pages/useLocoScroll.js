2630e1870ecf7593eea28b196e73bca39b87e26f
W Rivera <wrivera@Ws-iMac.fios-router.home>
2021-09-20 18:40:02 -0400

    locoFinally

--- pages/useLocoScroll.js
+++ pages/useLocoScroll.js
@@ -1,69 +1,146 @@
- import gsap from "gsap";
-import ScrollTrigger from "gsap/dist/ScrollTrigger";
-import { useEffect } from "react";
+/* eslint-disable @next/next/no-img-element */
+ 
+import { useRef, useEffect } from "react";
+import gsap from 'gsap'
 
-export default function useLocoScroll() {
+{{ /* REFERENCE: https://codesandbox.io/s/10vn0?file=/styles/styles.css:0-906  
+
+  1. Create a constant with a unique name....  
+  2. Create a constant name for a useRef 
+  3. Create a useEffect with a conditional which includes typeof window === "undefined" and return it  
+  4. Create a constant name to hold the locomomotive IMPORTwhich includes new LocomotiveScroll .default
+  5. Scroll destroy it
+  5b. Parent div call useRef by name
+  6. Export the constant
+*/
+}}
+
+const Snapp = () => {
+ 
+  const scrollRef = useRef();
+
   useEffect(() => {
-    gsap.registerPlugin(ScrollTrigger);
-    let locoScroll = null;
-
-    const scrollEl = document.querySelector("#smooth-scroll");
+    if (typeof window === "undefined") {
+      return;
+    }
 
     const scroll = import("locomotive-scroll").then((LocomotiveScroll) => {
       new LocomotiveScroll.default({
-        el: scrollEl,
-        smooth: true,
-        lerp: 0.075,
-        multiplier: 0.9
+        el: scrollRef.current,
+        smooth: true
       });
     });
-    
- /*   locoScroll = new LocomotiveScroll({
-      el: scrollEl,
-      smooth: true,
-      lerp: 0.075,
-      multiplier: 0.9
-    });*/
 
-    document.addEventListener("scroll", ScrollTrigger.update);
+ //   return () => scroll.destroy();
+  }, []);
 
-    ScrollTrigger.scrollerProxy(scrollEl, {
-      scrollTop(value) {
-        if (locoScroll) {
-          return arguments.length
-            ? locoScroll.scrollTo(value, 0, 0)
-            : locoScroll.scroll.instance.scroll.y;
-        }
-        return null;
-      },
-      getBoundingClientRect() {
-        return {
-          top: 0,
-          left: 0,
-          width: window.innerWidth,
-          height: window.innerHeight
-        };
-      },
+  return (
+    <div className="App" ref={scrollRef}>
+      <h2>Scroll and Fade in React</h2>
+      <h4 data-scroll
+          data-scroll-speed="2"
+          data-scroll-position="top"
+          data-scroll-direction="horizontal" >None</h4>
+      <h4>Something</h4>
+      <h4 data-scroll data-scroll-speed="1" style={{ marginTop: "2.65rem" }}>
+        Something Else
+      </h4>
+      <img  data-scroll data-scroll-speed="1"
+            className="greensock-icon"
+            height="874"
+            src="https://source.unsplash.com/random/1240x874?sig=76"
+            width="1240"
+            alt=""
+          />
+      <div data-scroll data-scroll-speed="4" data-scroll-position="middle" data-scroll-direction="horizontal">
+        <img  
+          className="greensock-icon"
+          height="874"
+          src="https://source.unsplash.com/random/1240x874?sig=76"
+          width="1240"
+          alt=""
+        />
+      </div>
+<div data-scroll data-scroll-speed="-5" data-scroll-position="bottom" data-scroll-direction="horizontal">
 
-      pinType: scrollEl.style.transform ? "transform" : "fixed"
-    });
+  <div>       
+    <img 
+            className="greensock-icon"
+            height="874"
+            src="https://source.unsplash.com/random/1240x874?sig=76"
+            width="1240"
+            alt=""
+          />
+  </div>
+  <div>9 afasdf asdfasdfasdf asfasdfasdfasasdfasd a sdfas a asdfas asdf</div>
+</div>
 
-    const lsUpdate = () => {
-      if (locoScroll) {
-        locoScroll.update();
-      }
-    };
+      <style
+      dangerouslySetInnerHTML={{
+          __html: `
+          .App {
+            font-family: sans-serif;
+            text-align: center;
+            height: 300vh;
+            padding-bottom: 300vh;
+          }
+          
+          html.has-scroll-smooth {
+            overflow: hidden;
+          }
+          
+          html.has-scroll-dragging {
+            -webkit-user-select: none;
+            -moz-user-select: none;
+            -ms-user-select: none;
+            user-select: none;
+          }
+          
+          .has-scroll-smooth body {
+            overflow: hidden;
+          }
+          
+          .has-scroll-smooth [data-scroll-container] {
+            min-height: 100vh;
+          }
+          
+          .c-scrollbar {
+            position: absolute;
+            right: 0;
+            top: 0;
+            width: 11px;
+            height: 100vh;
+            transform-origin: center right;
+            transition: transform 0.3s, opacity 0.3s;
+            opacity: 0;
+          }
+          .c-scrollbar:hover {
+            transform: scaleX(1.45);
+          }
+          .c-scrollbar:hover,
+          .has-scroll-scrolling .c-scrollbar,
+          .has-scroll-dragging .c-scrollbar {
+            opacity: 1;
+          }
+          
+          .c-scrollbar_thumb {
+            position: absolute;
+            top: 0;
+            right: 0;
+            background-color: hotpink;
+            opacity: 0.5;
+            width: 7px;
+            border-radius: 10px;
+            margin: 2px;
+          }
+          
+          
+     
+                   `
+      }}
+    />
+    </div>
+  );
+};
 
-    ScrollTrigger.addEventListener("refresh", lsUpdate);
-    ScrollTrigger.refresh();
-
-    return () => {
-      if (locoScroll) {
-        ScrollTrigger.removeEventListener("refresh", lsUpdate);
-        locoScroll.destroy();
-        locoScroll = null;
-        console.log("Kill", locoScroll);
-      }
-    };
-  });
-}
+export default Snapp;
