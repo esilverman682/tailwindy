48fcda70912349986d26d7e900a9d4e8f7b408b7
W Rivera <wrivera@Ws-iMac.fios-router.home>
2021-09-20 21:41:48 -0400

    locoLerp

--- pages/Intro.js
+++ pages/Intro.js
@@ -1,120 +1,146 @@
-import React from "react";
+/* eslint-disable @next/next/no-img-element */
+ 
+import { useRef, useEffect } from "react";
+import gsap from 'gsap'
 
-import { motion, AnimatePresence } from "framer-motion";
+{{ /* REFERENCE: https://codesandbox.io/s/10vn0?file=/styles/styles.css:0-906  
 
+  1. Create a constant with a unique name....  
+  2. Create a constant name for a useRef 
+  3. Create a useEffect with a conditional which includes typeof window === "undefined" and return it  
+  4. Create a constant name to hold the locomomotive IMPORTwhich includes new LocomotiveScroll .default
+  5. Scroll destroy it
+  5b. Parent div call useRef by name
+  6. Export the constant
+*/
+}}
 
-{{/*https://codesandbox.io/s/psimple-intro-v40t8?file=/src/styles.css:0-445 */}}
-export default function App() {
+const Snapp = () => {
+ 
+  const scrollRef = useRef();
+
+  useEffect(() => {
+    if (typeof window === "undefined") {
+      return;
+    }
+
+    const scroll = import("locomotive-scroll").then((LocomotiveScroll) => {
+      new LocomotiveScroll.default({
+        el: scrollRef.current,
+        smooth: true
+      });
+    });
+
+ //   return () => scroll.destroy();
+  }, []);
+
   return (
-    <motion.div
-      className="logo"
-      initial={{ scale: 60, rotate: -5 }}
-      animate={{ scale: 1, rotate: 0 }}
-      transition={{
-        type: "spring",
-        stiffness: 100,
-        damping: 20,
-        duration: 0.5
-      }}
-    >
-      <svg
-        className="top"
-        width="76"
-        height="94"
-        viewBox="0 0 76 94"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path
-          d="M75.1119 37.556C75.1119 16.8144 58.2976 0 37.556 0H34.3134C15.3626 0 0 15.3626 0 34.3134L4.50341e-06 93.8419C0.0150999 75.112 38.1462 75.1074 38.1462 75.1074C58.6156 74.792 75.1119 58.1004 75.1119 37.556Z"
-          fill="white"
+    <div className="App" ref={scrollRef}>
+      <h2>Scroll and Fade in React</h2>
+      <h4 data-scroll
+          data-scroll-speed="2"
+          data-scroll-position="top"
+          data-scroll-direction="horizontal" >None</h4>
+      <h4>Something</h4>
+      <h4 data-scroll data-scroll-speed="1" style={{ marginTop: "2.65rem" }}>
+        Something Else
+      </h4>
+      <img  data-scroll data-scroll-speed="1"
+            className="greensock-icon"
+            height="874"
+            src="https://source.unsplash.com/random/1240x874?sig=76"
+            width="1240"
+            alt=""
+          />
+      <div data-scroll data-scroll-speed="4" data-scroll-position="middle" data-scroll-direction="horizontal">
+        <img  
+          className="greensock-icon"
+          height="874"
+          src="https://source.unsplash.com/random/1240x874?sig=76"
+          width="1240"
+          alt=""
         />
-      </svg>
+      </div>
+<div data-scroll data-scroll-speed="-5" data-scroll-position="bottom" data-scroll-direction="horizontal">
 
-      <svg
-        className="bottom"
-        width="39"
-        height="39"
-        viewBox="0 0 39 39"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path
-          d="M38.4743 19.2802C38.4743 29.9159 30.5795 38.5378 19.2167 38.5378C8.58108 38.5378 0.328085 30.2002 0.328085 19.2802C0.328085 0.539114 38.4743 0.539093 38.4743 0.539093C38.4743 0.539093 38.4743 8.64454 38.4743 19.2802Z"
-          fill="#C4C4C4"
-        />
-        <path
-          d="M38.4743 19.2802C38.4743 29.9159 30.5795 38.5378 19.2167 38.5378C8.58108 38.5378 0.328085 30.2002 0.328085 19.2802C0.328085 0.539114 38.4743 0.539093 38.4743 0.539093C38.4743 0.539093 38.4743 8.64454 38.4743 19.2802Z"
-          fill="url(#paint0_linear)"
-        />
-        <path
-          d="M38.4743 19.2802C38.4743 29.9159 30.5795 38.5378 19.2167 38.5378C8.58108 38.5378 0.328085 30.2002 0.328085 19.2802C0.328085 0.539114 38.4743 0.539093 38.4743 0.539093C38.4743 0.539093 38.4743 8.64454 38.4743 19.2802Z"
-          fill="url(#paint1_linear)"
-        />
-        <defs>
-          <linearGradient
-            id="paint0_linear"
-            x1="33.5405"
-            y1="38.4752"
-            x2="15.722"
-            y2="6.2191"
-            gradientUnits="userSpaceOnUse"
-          >
-            <stop stopColor="#BCFFFF" />
-            <stop offset="1" stopColor="#BCFFFF" />
-          </linearGradient>
-          <linearGradient
-            id="paint1_linear"
-            x1="-24.7303"
-            y1="-30.984"
-            x2="35.8568"
-            y2="29.2283"
-            gradientUnits="userSpaceOnUse"
-          >
-            <stop stopColor="#3449D3" />
-            <stop offset="1" stopColor="#B6BFFF" />
-          </linearGradient>
-        </defs>
-      </svg>
+  <div>       
+    <img 
+            className="greensock-icon"
+            height="874"
+            src="https://source.unsplash.com/random/1240x874?sig=76"
+            width="1240"
+            alt=""
+          />
+  </div>
+  <div>9 afasdf asdfasdfasdf asfasdfasdfasasdfasd a sdfas a asdfas asdf</div>
+</div>
+
       <style
       dangerouslySetInnerHTML={{
           __html: `
-
-          header {
-            display: none !important;
+          .App {
+            font-family: sans-serif;
+            text-align: center;
+            height: 300vh;
+            padding-bottom: 300vh;
           }
-          body {
-              background:radial-gradient(
-              89.35% 90.25% at 3.71% 6.39%,
-              #0d99e8 0%,
-              #710fd2 100%);
           
-            background-size: cover !important;
-            background-repeat: no-repeat;
-            height: 100vh;
-            max-height: 100vh;
+          html.has-scroll-smooth {
             overflow: hidden;
-            display: flex;
-            align-items: center;
-            justify-items: center;
-            justify-content: center;
-          }
-          .logo {
-            display: flex;
-            flex-direction: column;
-          }
-          .bottom {
-            position: relative;
-            top: -12px;
-            left: 200px;
           }
           
+          html.has-scroll-dragging {
+            -webkit-user-select: none;
+            -moz-user-select: none;
+            -ms-user-select: none;
+            user-select: none;
+          }
+          
+          .has-scroll-smooth body {
+            overflow: hidden;
+          }
+          
+          .has-scroll-smooth [data-scroll-container] {
+            min-height: 100vh;
+          }
+          
+          .c-scrollbar {
+            position: absolute;
+            right: 0;
+            top: 0;
+            width: 11px;
+            height: 100vh;
+            transform-origin: center right;
+            transition: transform 0.3s, opacity 0.3s;
+            opacity: 0;
+          }
+          .c-scrollbar:hover {
+            transform: scaleX(1.45);
+          }
+          .c-scrollbar:hover,
+          .has-scroll-scrolling .c-scrollbar,
+          .has-scroll-dragging .c-scrollbar {
+            opacity: 1;
+          }
+          
+          .c-scrollbar_thumb {
+            position: absolute;
+            top: 0;
+            right: 0;
+            background-color: hotpink;
+            opacity: 0.5;
+            width: 7px;
+            border-radius: 10px;
+            margin: 2px;
+          }
           
           
      
                    `
       }}
     />
-    </motion.div>
+    </div>
   );
-}
+};
+
+export default Snapp;
